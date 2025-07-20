6db6ecd7367e89e63d91e0997152be2763a36c07
PODDAe <camalkaakash2@gmail.com>
2025-07-20 08:46:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
+  SESSION_ID: process.env.SESSION_ID || "VTFiSDRL#O9n3w7N4BPh5cijkDixKdAElgEIWeYmOAni4IWzjKxc",
   MONGODB: process.env.MONGODB || "Enter your mongoDB public URL",
-  OWNER_NUM: process.env.OWNER_NUM || "9470xxxxxxx",
+  OWNER_NUM: process.env.OWNER_NUM || "94752978237",
 };
